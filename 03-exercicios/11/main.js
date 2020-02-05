fe89b82793d6cbd31ecb8403ef8ee603f6a51a4f
lailson henrique <lailson.pi@gmail.com>
2020-02-05 19:25:38 -0300

    adicionando css

--- 03-exercicios/11/main.js
+++ 03-exercicios/11/main.js
@@ -1,3 +1,9 @@
-function mostrar(){
-  alert(document.getElementById('nome').value);
+var botao = document.querySelector('button');
+var input = document.querySelector('input');
+
+function mostraMensagem(){
+  alert(input.value);
 }
+
+botao.onclick = mostraMensagem;
+botao.style.borderColor = 'blue'
